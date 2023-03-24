a47b0c289971e61c9a9b9ab784713dfd9de2ae50
GUANTAI001 <jpguantai@gmail.com>
2023-03-24 22:41:32 +0300

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Hero.create(name: "Batman", super_name: "Bruce Wayne")
+Hero.create(name: "Superman", super_name: "Clark Kent")
+
+Power.create(name: "Super Strength", description: "Superman can lift 100 tons")
+Power.create(name: "Super Speed", description: "Superman can run at 670 mph")
+
+HeroPower.create(hero_id: 1, power_id: 1, strength: "Weak")
+HeroPower.create(hero_id: 1, power_id: 2, strength: "Strong")
+HeroPower.create(hero_id: 2, power_id: 1, strength: "Strong")
